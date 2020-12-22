b5a001e197fe80eb0e424a64a88a03e27e664750
Lorenz Walthert <l.walthert@urbandatalab.net>
2020-12-22 13:38:56 +0100

    actually not related to testthat

--- tests/testthat/test-segfault.R
+++ tests/testthat/test-segfault.R
@@ -3,18 +3,27 @@ test_that("int vector of 0 with length one are equal", {
   expect_equal(integer(1), 0L)
 })
 
-test_that("works", {
+f1 <- function(...) {
   withr::defer(2)
   data <- dplyr::slice(mtcars, 0L)
-})
+}
+
+f1()
 
 
-test_that("works", {
+f2 <- function() {
   data <- dplyr::slice(mtcars, integer(1))
-})
+}
 
-test_that("does not work", {
+f2()
+
+
+
+f3 <- function(...) {
   withr::defer(2)
   integer(1) # works
+  message('reaching this')
   data <- dplyr::slice(mtcars, integer(1))
-})
+}
+
+f3()
